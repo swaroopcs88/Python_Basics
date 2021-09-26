a845d05e58fd0b71cb8edddd4670e2c72d1b4a88
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 13:36:14 +0530

    Update OOPracticals1a.py

--- OOPracticals1a.py
+++ OOPracticals1a.py
@@ -1,9 +1,11 @@
+# https://www.geeksforgeeks.org/how-to-create-an-empty-class-in-python/
+# Create an empty class named Actor
 class Actor:
     pass
  
- 
+# Create an object of Actor (a1 = Actor())
 a1 = Actor()
 print(a1)
- 
+# Type a1.name = ‘Ranveer’ and print it to console. Do you see the name printed?
 a1.name = "Ranveer"
 print(a1.name)
