ff564a558934822f0c555c45e9d506a98e34e619
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 13:50:37 +0530

    Update Second.py

--- Second.py
+++ Second.py
@@ -1,22 +1,25 @@
+# python program to check whether the current second is odd or not based on the system time stamp
+# import datetime from the datetime module
 from datetime import datetime
+# importing the modules which are used in this program
 import random
 import time
 
-
+# declaring the variable odds
 odds = [1,2,3,4,9,11,23,13,42,34,53,44]
 
-
+# number of times you are going to check whether the current second is odd or even
 times = 5
 
 print('odds = '+str(odds))
-for x in range(0, times):
+# number of times x is going to iterate 
+for x in range(0, times):   
     pauseSecs = random.randint(0,x)
     time.sleep(pauseSecs)
     currSec = datetime.today().second
     print('currSec = '+str(currSec))
     if currSec in odds:
         print('this is an odd moment ' +str(x))
-    
     else:
         print('this is not an odd moment! '+str(x))
  
