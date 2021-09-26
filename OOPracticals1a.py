a9341b971466caa30387cd75dc7846acffa9ab6c
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 21:12:15 +0530

    Update OOPracticals1a.py

--- OOPracticals1a.py
+++ OOPracticals1a.py
@@ -1,9 +1,10 @@
+# declare a empty class Actor
 class Actor:
     pass
- 
+ # create an object of the class Actor
  
 a1 = Actor()
 print(a1)
- 
+ # create an instance of the class Actor
 a1.name = "Ranveer"
 print(a1.name)
