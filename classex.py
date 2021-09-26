ba1b5dce1a680dda04f5a390584b33ba63382f2d
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 14:33:20 +0530

    Update classex.py

--- classex.py
+++ classex.py
@@ -1,10 +1,11 @@
-"""
+"""example1
+# declare empty class customer
 
 class customer:
     pass
-
+# create an object of the class customer
 customer1 = customer()
-
+# create an instance of the class customer
 customer1.first_name = 'John'
 customer1.phone_number = 9999999988
 
@@ -18,7 +19,9 @@ print('customer1:', customer1.first_name, customer1.phone_number)
 print('customer2:', customer2.first_name, customer2.mobile_number)
 """
 
-"""
+
+
+"""example2
 class Employee:
     pass
 
@@ -30,6 +33,13 @@ e1.name = "John Doe"
 print(e1.name)
 """
 
+
+
+
+
+
+
+"""example3
 class Actor:
     pass
 
@@ -40,5 +50,6 @@ a1.name = 'Ranveer'
 print(a1.name)
 
 a2 = Actor()
-
+a2.name = 'Ranbir'
 print(a2.name)
+"""
