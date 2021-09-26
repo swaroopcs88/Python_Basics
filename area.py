7f6e822a641378ee0cb6db556ea8cf141ad37ae8
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 13:54:19 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,5 +1,5 @@
 # to find area of a circle
-import math # here math modue is imported
+import math # here math module is imported
 r = float(input('Enter the radius: '))
 area = math.pi * r**2 # pi is a constant in math module
 print('Area of circle= ', area)
