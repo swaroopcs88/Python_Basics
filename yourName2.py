762f7b0028fdb79641aaca6a4527f8da4f6b560a
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 21:52:42 +0530

    Update yourName2.py

--- yourName2.py
+++ yourName2.py
@@ -1,6 +1,6 @@
-while True:
+while True: # executes the stmts while stmt is true
     print('Please type your name.')
     name = input()
-    if name == 'your name':
+    if name == 'your name': # until the name is equivalent to your name. this code won't break and come out of the while.
         break
 print('Thank you!')
