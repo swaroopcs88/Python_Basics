8a5150670b3a74e81dd6525a1571a4a538fb2ce6
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 21:05:31 +0530

    Update Alice.py

--- Alice.py
+++ Alice.py
@@ -1,9 +1,9 @@
-name = 'Bob'
-age = 22
-if name == 'Alice':
+name = 'Bob' # declare a variable name
+age = 22 # declare a variable age
+if name == 'Alice': # check if the name is Alice, if true, print the result to the monitor
     print('Hi, ' + name)
-elif age < 12:
+elif age < 12: # check if the age is less than 12
     print('You are not Alice, Kiddo!')
 else:
-    print('Do nothing!')
+    print('Do nothing!') # this is printed. since, both the above cases fail
     
