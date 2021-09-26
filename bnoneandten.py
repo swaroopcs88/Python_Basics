e289c36d225348ccc8869b3efba750cb33d795b4
Swaroop Dharmappa <41771221+swaroopcs88@users.noreply.github.com>
2021-09-26 20:52:52 +0530

    Update bnoneandten.py

--- bnoneandten.py
+++ bnoneandten.py
@@ -4,9 +4,9 @@ print(x)
 print('Illi')
 
 if x >= 1 and x <= 10:
-    print("You typed", x, "Which is between 1 and 10")
-elif:
-    print("You typed", x, "Which is below 1 or above 10")
+        print("You typed", x, "Which is between 1 and 10")
+elif x <= 1 or x >= 10:
+        print("You typed", x, "Which is below 1 or above 10")
 else:
-    print("inelli")
-    
+        print("inelli")
+
